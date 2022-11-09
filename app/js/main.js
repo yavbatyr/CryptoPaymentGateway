ff6b729ac335e0aa6a48b3e3bb3ff5244b449244
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-09 18:41:40 +0300

    created section 'features'

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,30 @@
 $(function(){
 
-})
+});
+
+const swiper1 = new Swiper('.swiper', {
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+      
+    },
+    minimumVelocity: 1,
+    momentumVelocityRatio: 2,
+    momentum: true,
+    loop: true,
+    speed: 8000,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+});
+const swiper2 = new Swiper('.swiper-bottom ', {
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+      reverseDirection: true,
+      momentum: true,
+    },
+    loop: true,
+    speed: 7777,
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+});
