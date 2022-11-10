49930b0b3f5573a0b8ecd1cc2fd469f4dff88d0a
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-10 16:11:44 +0300

    made the swiper a little smoother

--- app/js/main.js
+++ app/js/main.js
@@ -2,26 +2,11 @@ $(function(){
 
 });
 
-const swiper1 = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper', {
     autoplay: {
-      delay: 0,
+      delay: 1,
       disableOnInteraction: false,
-      
-    },
-    minimumVelocity: 1,
-    momentumVelocityRatio: 2,
-    momentum: true,
-    loop: true,
-    speed: 10000,
-    slidesPerView: 'auto',
-    spaceBetween: 30,
-});
-const swiper2 = new Swiper('.swiper-bottom ', {
-    autoplay: {
-      delay: 0,
-      disableOnInteraction: false,
-      reverseDirection: true,
-      momentum: true,
+      //  reverseDirection: true,
     },
     loop: true,
     speed: 10000,
