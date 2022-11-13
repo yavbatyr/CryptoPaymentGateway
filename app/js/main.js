bdf6eff671160a8e68959b86182f6fe6f83a003e
yavbatyr <95848367+yavbatyr@users.noreply.github.com>
2022-11-13 14:16:57 +0300

    added function content disclosure for 'difference__btn'

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,8 @@
 $(function(){
-
+  $('.difference__open-btn').on('click', function () { 
+    $('.difference__wrapper').removeClass('difference__wrapper-hidden');
+    $(this).removeClass('difference__btn');
+  });
 });
 
 const swiper = new Swiper('.swiper', {
